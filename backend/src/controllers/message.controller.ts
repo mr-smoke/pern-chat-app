318727868bb0b9e620c58c0a313b7374717283c2
baki duman <g.s_baki_70@hotmail.com>
2024-10-04 13:52:59 +0300

    getConversations controller added.

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -54,3 +54,59 @@ export const sendMessage = async (req: Request, res: Response) => {
         res.status(500).send(error.message);
     }
 };
+
+export const getMessages = async (req: Request, res: Response) => {
+    try {
+        const { id: receiverId } = req.params;
+        const { id: senderId } = req.user;
+
+        let conversation = await prisma.conversation.findFirst({
+            where: {
+                usersIds: {
+                    hasEvery: [senderId, receiverId],
+                },
+            },
+            include: {
+                messages: {
+                    orderBy: {
+                        createdAt: 'asc',
+                    },
+                },
+            },
+        });
+
+        if (!conversation) {
+            return res.status(404).json({ error: 'No conversation found between these users' });
+        }
+
+        res.status(200).json({ messages: conversation.messages });
+        return null as any;
+    }
+    catch (error: any) {
+        res.status(500).send(error.message);
+    }
+};
+
+export const getConversations = async (req: Request, res: Response) => {
+    try {
+        const { id: userId } = req.user;
+
+        const conversations = await prisma.user.findMany({
+            where: {
+                id: {
+                    not: userId,
+                },
+            },
+            select: {
+                id: true,
+                name: true,
+                profilePic: true,
+            },
+        });
+
+        res.status(200).json({ conversations });
+    }
+    catch (error: any) {
+        res.status(500).send(error.message);
+    }
+};
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import protectRoute from '../middleware/protectRoute.js';
-import { sendMessage } from '../controllers/message.controller.js';
+import { getConversations, getMessages, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage)
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/conversations", protectRoute, getConversations);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
