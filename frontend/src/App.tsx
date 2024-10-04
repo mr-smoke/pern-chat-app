4085dccc8b9d9110936069cd11d0864b0c02575d
baki duman <g.s_baki_70@hotmail.com>
2024-10-04 17:35:24 +0300

    AuthContext added for authentication process.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const { authUser, setAuthUser, isLoading } = useAuth();
+
+  console.log(authUser);
   return (
     <>
       <Routes>
---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,64 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type AuthUser = {
+  id: string;
+  username: string;
+  name: string;
+  profilePic: string;
+  gender: string;
+};
+
+const AuthContext = createContext<{
+  authUser: AuthUser | null;
+  setAuthUser: Dispatch<SetStateAction<AuthUser | null>>;
+  isLoading: boolean;
+}>({
+  authUser: null,
+  setAuthUser: () => null,
+  isLoading: true,
+});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [authUser, setAuthUser] = useState<AuthUser | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("/api/auth/getAuth");
+        const data = await response.json();
+
+        if (response.ok) {
+          setAuthUser(data);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
