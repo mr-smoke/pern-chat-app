9d2579bbbff8675d39cb93dc5c6e13c06c7d003c
baki duman <g.s_baki_70@hotmail.com>
2024-10-06 11:19:59 +0300

    UI design messages section.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import useGetMessages from "../hooks/useGetMessages";
 import useLogout from "../hooks/useLogout";
 import useConversation from "../zustand/useConversation";
 import useSendMessage from "../hooks/useSendMessage";
-import { FaSearch, FaSignOutAlt } from "react-icons/fa";
+import { FaPaperPlane, FaSearch, FaSignOutAlt } from "react-icons/fa";
 
 const Home = () => {
   const { handleLogout } = useLogout();
@@ -24,13 +24,13 @@ const Home = () => {
   };
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="border rounded-xl flex w-full h-full md:w-2/3 md:h-3/4 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 text-white">
+      <div className="border rounded-xl flex w-full h-full md:w-2/3 md:h-3/4 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 text-white overflow-hidden">
         <div className="flex flex-col flex-1 w-1/3 border-r-2">
           <div className="border-b-2 p-4 flex items-center gap-3">
             <input
               className="p-3 rounded-lg bg-slate-700 flex-1"
               type="text"
-              placeholder="Search"
+              placeholder="Search..."
             />
             <button className="rounded-full bg-slate-700 p-3">
               <FaSearch className="w-5 h-5" />
@@ -48,7 +48,7 @@ const Home = () => {
                   alt={conversation.name}
                   className="w-10 h-10 rounded-full"
                 />
-                <span>{conversation.name}</span>
+                <span className="font-semibold">{conversation.name}</span>
               </div>
             ))}
           </div>
@@ -56,24 +56,32 @@ const Home = () => {
             <FaSignOutAlt size={30} />
           </button>
         </div>
-        <div className="flex flex-col gap-3 w-2/3 p-3">
-          <h1 className="text-xl">Chat with {selectedConversation?.name}</h1>
-          <div className="border rounded-xl p-3 h-96 overflow-y-auto">
+        <div className="flex flex-col w-2/3">
+          <h1 className="text-xl font-bold border-b-2 p-3">
+            Chat with {selectedConversation?.name}
+          </h1>
+          <div className="rounded-xl p-3 h-96 overflow-y-auto  flex-1">
             {isLoading && <p>Loading...</p>}
             {!isLoading &&
               messages.map((message) => (
-                <div key={message.id}>
-                  <span>{message.content}</span>
+                <div className="p-1" key={message.id}>
+                  <p className="p-3 rounded-xl bg-slate-700 w-max">
+                    {message.content}
+                  </p>
                 </div>
               ))}
           </div>
-          <form onSubmit={messageHandler}>
+          <form className="p-4 flex gap-3" onSubmit={messageHandler}>
             <input
+              className="flex-1 p-3 rounded-lg bg-slate-700"
               type="text"
               onChange={(e) => setMessage(e.target.value)}
               value={message}
+              placeholder="Type a message"
             />
-            <button type="submit">Send</button>
+            <button className="rounded-full bg-slate-700 p-3" type="submit">
+              <FaPaperPlane className="w-5 h-5" />
+            </button>
           </form>
         </div>
       </div>
